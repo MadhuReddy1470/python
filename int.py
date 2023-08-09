7b48f7b01fe987d6b66497c65de680533e70a5f4
MadhuReddy1470 <madhulathareddy1470@gmail.com>
2023-08-09 16:07:47 +0530

    update

---
+++ int.py
@@ -0,0 +1,7 @@
+age = 20
+age += 2
+print(age)
+print(type(age))
+#here age + inicates how much we are adding to the given age
+print("your age is :" + str(age))
+#again here we use str because we cannot add string and int 
