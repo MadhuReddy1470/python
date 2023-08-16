69a1b7ac4d12926139a9ca625f966e363c09d764
MadhuReddy1470 <madhulathareddy1470@gmail.com>
2023-08-16 14:32:57 +0530

    update

--- multiple3.py
+++ multiple3.py
@@ -1,7 +1,11 @@
-name = "bro code"
+name = "brocode"
 print(len(name))
 print(name.capitalize())
 print(name.find("o"))
-print(name.capitalize())
 print(name.upper())
 print(name.lower())
+print(name.count("o"))
+print(name.isdigit())
+print(name.isalpha())
+print(name*3)
+#capithalize gives 1st letter cap,upper gives all cap,lower gives all small,count gives no of particular letter,is digit gives if there are numbers,isalpha gives true if there are no space
